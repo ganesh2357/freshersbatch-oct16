b2eef366267dca426c9bda1f8b015965a62d6c54
Ganesh Botcha <ganesh.botcha@capgemini.com>
2022-06-19 19:45:47 +0530

    New Assignments Added

---
+++ java Assignments/core java assignments/src/com/bg/Validation.java
@@ -0,0 +1,32 @@
+package com.bg;
+
+import java.util.Scanner;
+
+public class Validation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String user;
+		String pass;
+		int i = 0;
+		for (i = 0; i < 3;) {
+			System.out.println("Enter user name");
+			user = sc.next();
+			System.out.println("Enter Password");
+			pass = sc.next();
+
+			if (user.length() < 4 || pass.length() > 6) {
+				System.out.println("Invalid Credential");
+				i++;
+			} else {
+				System.out.println("Welcome");
+				break;
+			}
+		}
+		if (i == 3) {
+			System.out.println("Contact Admin");
+		}
+
+	}
+
+}
