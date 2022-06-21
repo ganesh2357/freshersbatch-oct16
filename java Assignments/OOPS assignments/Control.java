692b47c4212ac6bc44afdf8f98015b4a43d6a987
Ganesh Botcha <ganesh.botcha@capgemini.com>
2022-06-21 18:00:30 +0530

    oops assignments added

---
+++ java Assignments/OOPS assignments/Control.java
@@ -0,0 +1,29 @@
+package Oops;
+
+abstract class persistence {
+	abstract void persist();
+}
+
+class filePersistence extends persistence {
+	void persist() {
+		System.out.println("file persists");
+	}
+}
+
+class databasePersistence extends persistence {
+	void persist() {
+		System.out.println("database persists");
+	}
+}
+
+public class Control {
+	public static void main(String[] args) {
+		persistence obj = new persistence() {
+			@Override
+			void persist() {
+				System.out.println("calling it");
+			}
+		};
+		obj.persist();
+	}
+}
