d377658161e1c959020c5e304964bf296310e694
Ganesh Botcha <ganesh.botcha@capgemini.com>
2022-06-25 15:14:10 +0530

    Assignments are added

---
+++ java Assignments/A10/SimpleInterest.java
@@ -0,0 +1,19 @@
+package A10;
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter your Principle, rate and time: ");
+		int p = s.nextInt();
+		int r = s.nextInt();
+		int t = s.nextInt();
+		calculate simple_interest = (a, b, c) -> System.out.println("The simple interest: $ " + (a * b * c) * 0.01);
+		simple_interest.give(p, r, t);
+	}
+}
+
+interface calculate {
+	public void give(int p, int r, int t);
+}
